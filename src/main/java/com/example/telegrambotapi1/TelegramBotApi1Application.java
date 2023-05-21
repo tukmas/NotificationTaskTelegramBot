dbea8de45f5830a2541ac10f164430f5f96f5c8a
Сергей Николаевич <tukmachev.18@yandex.ru>
2023-05-21 14:34:14 +0300

    Первый коммит

---
+++ src/main/java/com/example/telegrambotapi1/TelegramBotApi1Application.java
@@ -0,0 +1,13 @@
+package com.example.telegrambotapi1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotApi1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotApi1Application.class, args);
+    }
+
+}
