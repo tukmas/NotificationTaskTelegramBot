98dc86c92e2de0c1e3fbb66088f5fa4d21a69b71
Сергей Николаевич <tukmachev.18@yandex.ru>
2023-05-21 14:14:40 +0300

    Первый коммит

---
+++ src/test/java/com/example/telegrambotapi1/TelegramBotApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telegrambotapi1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApi1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
